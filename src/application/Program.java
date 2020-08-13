d0792fb66bf3ceab4cd4c56a1d074689493f62e6
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2020-08-13 19:19:34 +0100

    Better implementation WITH interface default method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import services.BrazilInterestService;
+import services.InterestService;
 import services.UsaInterestService;
 
 import java.util.Locale;
@@ -19,7 +20,7 @@ public class Program {
         System.out.println("Months: ");
         int months = scanner.nextInt();
 
-        BrazilInterestService brazilInterestService = new BrazilInterestService(2.0);
+        InterestService brazilInterestService = new BrazilInterestService(2.0);
         double paymentBrazil = brazilInterestService.payment(amount, months);
 
         System.out.println("Payment after " + months + " months - Brazil");
@@ -27,7 +28,7 @@ public class Program {
 
         System.out.println();
 
-        UsaInterestService usaInterestService = new UsaInterestService(1.0);
+        InterestService usaInterestService = new UsaInterestService(1.0);
         double paymentUsa = usaInterestService.payment(amount, months);
 
         System.out.println("Payment after " + months + " months - USA");
--- src/services/BrazilInterestService.java
+++ src/services/BrazilInterestService.java
@@ -2,7 +2,7 @@ package services;
 
 import java.security.InvalidParameterException;
 
-public class BrazilInterestService {
+public class BrazilInterestService implements InterestService {
 
     private double interestRate;
 
@@ -10,14 +10,9 @@ public class BrazilInterestService {
         this.interestRate = interestRate;
     }
 
+    @Override
     public double getInterestRate() {
         return interestRate;
     }
 
-    public double payment(double amount, int months) {
-        if (months < 1) {
-            throw new InvalidParameterException("Months must be greater than zero");
-        }
-        return amount * Math.pow(1.0 + getInterestRate() / 100, months);
-    }
 }
